e693a4e3204662c40756a6df386f5e01bf7f67aa
Muzi <muzi.phage@gmail.com>
2024-05-15 12:58:45 +0200

    Initial code commit

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
@@ -1,8 +1,13 @@
 package com.learnkafkastreams.launcher;
 
+import com.learnkafkastreams.topology.GreetingsTopology;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 
 import java.util.List;
 import java.util.Properties;
@@ -12,30 +17,47 @@ import java.util.stream.Collectors;
 public class GreetingsStreamApp {
 
     public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "greetings-app"); // Synonymous with Consumer groups
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
+        // Create topics with AdminClient
+        createTopics(properties, List.of(GreetingsTopology.GREETINGS, GreetingsTopology.GREETINGS_UPPERCASE));
+        Topology greetingsTopology = GreetingsTopology.buildTopology();
+
+        KafkaStreams kafkaStreams = new KafkaStreams(greetingsTopology, properties);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+
+        try {
+            kafkaStreams.start();
+        } catch (Exception exception) {
+            log.error("Exception in starting the stream: " + exception.getMessage());
+        }
 
     }
 
     private static void createTopics(Properties config, List<String> greetings) {
 
         AdminClient admin = AdminClient.create(config);
-        var partitions = 1;
-        short replication  = 1;
+        var partitions = 2;
+        short replication = 1;
 
         var newTopics = greetings
                 .stream()
-                .map(topic ->{
+                .map(topic -> {
                     return new NewTopic(topic, partitions, replication);
                 })
                 .collect(Collectors.toList());
 
         var createTopicResult = admin.createTopics(newTopics);
         try {
-           createTopicResult
+            createTopicResult
                     .all().get();
             log.info("topics are created successfully");
         } catch (Exception e) {
-            log.error("Exception creating topics : {} ",e.getMessage(), e);
+            log.error("Exception creating topics : {} ", e.getMessage(), e);
         }
     }
 }
---
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -0,0 +1,40 @@
+package com.learnkafkastreams.topology;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
+
+public class GreetingsTopology {
+    public static String GREETINGS = "greetings";
+    public static String GREETINGS_UPPERCASE = "greetings_uppercase";
+
+    // Holds the frame of the application, source-process-sink
+    public static Topology buildTopology() {
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        // Source processor - Read form a provided GREETINGS topic
+        KStream<String, String> greetingsStream = streamsBuilder
+                .stream(GREETINGS, Consumed.with(Serdes.String(), Serdes.String()));
+
+        // Print the Original stream
+        greetingsStream
+                .print(Printed.<String, String>toSysOut().withLabel("greetingsStream"));
+
+        // Processor the streaming records
+        KStream<String, String> modifiedStream = greetingsStream
+                .mapValues((readOnlyKey, value) -> value.toUpperCase());
+        // Print the transformed stream
+        modifiedStream
+                .print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
+
+        // Sink Processor - Publish to GREETINGS_UPPERCASE topic
+        modifiedStream
+                .to(GREETINGS_UPPERCASE, Produced.with(Serdes.String(), Serdes.String()));
+
+        return streamsBuilder.build();
+    }
+}
